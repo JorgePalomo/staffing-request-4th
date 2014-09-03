69d72fbb3d692d0b68c5585ee500609d88724aa1
JorgePalomo <jorge.palomo@4thsource.com>
2014-09-03 15:47:42 -0500

    tags ui added

---
+++ app/js/component/ui/tags.js
@@ -0,0 +1,15 @@
+'use strict';
+
+define(['flight/lib/component'], function(defineComponent) {
+
+    // export component constructor
+    return defineComponent(tags);
+
+    // component definition
+    function tags() {
+        this.after('initialize', function() {
+            this.$node.tagsinput();
+        });
+    }
+
+});
